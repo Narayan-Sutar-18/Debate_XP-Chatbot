bb6b29c4d30cc145ee84001a4c4940d4dacfc668
Narayan-Sutar-18 <narayangsutar8554@gmail.com>
2025-01-23 22:19:15 +0530

    Final Commit

---
+++ src/app/config.js
@@ -0,0 +1,11 @@
+import { createChatBotMessage } from "react-chatbot-kit";
+
+const config = {
+  botName: "Debate Sparring Partner",
+  initialMessages: [
+    createChatBotMessage(`"Hey there, ready to sharpen your debating skills? I'm your friendly debate sparring partner! Let's dive in. What's the topic on your mind today?"`)
+  ]
+}
+
+export default config
+
